8175700e6e6ee495256583af2d0793286a185adf
ekimlinger <ekimlinger@gmail.com>
2016-12-11 23:14:19 -0600

    Initial setup

---
+++ client/scripts/app.js
@@ -0,0 +1,11 @@
+var myApp = angular.module("myApp", ['ngMaterial', 'ngRoute']);
+  myApp.factory("MyService", ["$http",'$window','$filter', function($http, $window, $filter){
+    return {
+
+    };
+  }]);
+
+  myApp.controller("MyController", ["$scope", "$timeout", "$mdSidenav", "$log", function($scope, $timeout, $mdSidenav, $log) {
+
+  }]);
+  
