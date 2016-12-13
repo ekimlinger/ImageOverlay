3a28d4be7143a4e9c39a484d59b9d9557e9b94df
ekimlinger <ekimlinger@gmail.com>
2016-12-12 21:04:42 -0600

    Cleaned up file structure

--- server/app.js
+++ server/app.js
@@ -3,20 +3,35 @@ var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
 var router = require('./modules/index.js');
+var cloudinary = require('cloudinary');
+var access = require('../access-tokens.json');
 
+cloudinary.config({
+  cloud_name: access["cloud_name"],
+  api_key: ["api_key"],
+  api_secret: ["api_secret"]
+});
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
+app.post('/upload-image', function(req,res){
+  var image = req.body.
+  res.status(200).send("Posted image successfully")
+});
 
 
-app.use('/', router);
+app.get('/test',function(req,res){
+  var file = req.params[0] || "/views/index.html";
+  res.sendFile(path.join(__dirname,"/public/", file));
+});
 
 app.get("/*", function(req,res){
   var file = req.params[0] || "/views/index.html";
   res.sendFile(path.join(__dirname,"/public/", file));
+  // Send documentation on how to use API
 });
 
 app.set("port",(process.env.PORT || 3000));
